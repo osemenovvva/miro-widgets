a08b651cfc7a73bd6ff0dda73a9ea7079aa3b8b8
Olga Semenova <o.semenovvva@gmail.com>
2024-03-05 15:40:25 +0700

    add concurrent collections

--- src/main/java/com/olgasemenova/mirowidgets/MiroWidgetsApplication.java
+++ src/main/java/com/olgasemenova/mirowidgets/MiroWidgetsApplication.java
@@ -8,9 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication(scanBasePackages = {"controller","service","model", "repository"})
 @ComponentScan
 public class MiroWidgetsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MiroWidgetsApplication.class, args);
 	}
-
 }
